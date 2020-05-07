c14bee4c561f6ba5919bda54cd4d3988e14ff9d2
Migushthe2nd <Migushthe2nd@users.noreply.github.com>
2020-05-07 13:06:55 +0200

    Spectator command repeating member argument

--- commands/spectator.js
+++ commands/spectator.js
@@ -7,6 +7,7 @@ module.exports = class extends Command {
             name: 'spectator',
             description: 'Toggle spectator for a user. This will allow them to view the answers channels, unlike just keeping the Pubquiz visible.',
             runIn: ['text'],
+            extendedHelp: [' - spectator @member1', ' - spectator @member1 @member2 @member3'].join('\n'),
             usage: '<member:member> [...]',
             cooldown: 1,
 
@@ -20,7 +21,7 @@ module.exports = class extends Command {
         });
     }
 
-    async run (message, [member]) {
+    async run (message, [...members]) {
         const creatorId = message.author.id
         const guildId = message.channel.guild.id
 
@@ -32,56 +33,73 @@ module.exports = class extends Command {
 
         if (results) {
             if (results.guild_id === guildId) {
-                if (results.creator_id !== member.id) {
-                    const categoryChannel = await this.client.guilds.cache.get(guildId).channels.cache.get(results.category_channel_id);
-                    if (results.participants ? !results.participants.includes(member.id) : true) {
-                        if (results.spectators ? !results.spectators.includes(member.id) : true) {
-                            try {
-                                db.none(`
-                                    UPDATE pubquiz_sessions
-                                    SET spectators = array_append(spectators, $2)
-                                    WHERE session_uuid = $1
-                                `, [results.session_uuid, member.id])
+                const allResponses = []
+                const allPromises = []
+                const categoryChannel = this.client.guilds.cache.get(guildId).channels.cache.get(results.category_channel_id);
+                members.forEach(member => {
+                    const promise = new Promise(async resolve => {
+                        if (results.creator_id !== member.id) {
+                            if (results.participants ? !results.participants.includes(member.id) : true) {
+                                if (results.spectators ? !results.spectators.includes(member.id) : true) {
+                                    try {
+                                        db.none(`
+                                            UPDATE pubquiz_sessions
+                                            SET spectators = array_append(spectators, $2)
+                                            WHERE session_uuid = $1
+                                                AND NOT (spectators @> ARRAY[$2::bigint]);
+                                        `, [results.session_uuid, member.id])
 
-                                await categoryChannel.children.forEach(channel => {
-                                    if (channel.id !== results.controls_channel_id) {
-                                        channel.updateOverwrite(member, {
-                                            VIEW_CHANNEL: true,
-                                            SEND_MESSAGES: false,
-                                            ADD_REACTIONS: false
+                                        await categoryChannel.children.forEach(channel => {
+                                            if (channel.id !== results.controls_channel_id) {
+                                                channel.updateOverwrite(member, {
+                                                    VIEW_CHANNEL: true,
+                                                    SEND_MESSAGES: false,
+                                                    ADD_REACTIONS: false
+                                                })
+                                            }
                                         })
+                                        allResponses.push(`${member} **successfully added** as spectator!`)
+                                        resolve()
+                                    } catch (e) {
+                                        console.log(e)
+                                        allResponses.push(`Something **went wrong** while trying to add ${member} as spectator :/`)
+                                        resolve()
                                     }
-                                })
-                                message.reply(`${member} **successfully added** as spectator!`)
-                            } catch (e) {
-                                console.log(e)
-                                message.reply("Something **went wrong** while trying to add that user as spectator :/")
+                                } else {
+                                    try {
+                                        db.none(`
+                                            UPDATE pubquiz_sessions
+                                            SET spectators = array_remove(spectators, $2)
+                                            WHERE session_uuid = $1
+                                                -- AND (spectators @> ARRAY[$2::bigint]);
+                                        `, [results.session_uuid, member.id])
+
+                                        await categoryChannel.children.forEach(channel => {
+                                            if (channel.id !== results.controls_channel_id) {
+                                                channel.permissionOverwrites.get(member.id).delete();
+                                            }
+                                        })
+                                        allResponses.push(`${member} **successfully removed** as spectator!`)
+                                        resolve()
+                                    } catch (e) {
+                                        console.log(e)
+                                        allResponses.push(`Something **went wrong** while trying to remove ${member} as spectator :/`)
+                                        resolve()
+                                    }
+                                }
+                            } else {
+                                allResponses.push(`${member} is **a participant**.`)
+                                resolve()
                             }
                         } else {
-                            try {
-                                db.none(`
-                                    UPDATE pubquiz_sessions
-                                    SET spectators = array_remove(spectators, $2)
-                                    WHERE session_uuid = $1
-                                `, [results.session_uuid, member.id])
-
-                                await categoryChannel.children.forEach(channel => {
-                                    if (channel.id !== results.controls_channel_id) {
-                                        channel.permissionOverwrites.get(member.id).delete();
-                                    }
-                                })
-                                message.reply(`${member} **successfully removed** as spectator!`)
-                            } catch (e) {
-                                console.log(e)
-                                message.reply("Something **went wrong** while trying to remove that user as spectator :/")
-                            }
+                            allResponses.push(`You **can't add** yourself as spectator ;)`)
+                            resolve()
                         }
-                    } else {
-                        message.reply("That user is **a participant**.")
-                    }
-                } else {
-                    message.reply("You **can't add** yourself as spectator ;)")
-                }
+                    })
+                    allPromises.push(promise)
+                })
+                await Promise.all(allPromises);
+                message.reply(allResponses, { split: true })
             } else {
                 message.reply(`You must use this command in ${this.client.guilds.cache.get(results.guild_id)}.`)
             }
